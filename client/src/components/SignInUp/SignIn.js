936e46b8babc3b534efeed12bfe5521edec88184
Arpit Mathur <arpitmathur23@gmail.com>
2023-04-10 21:38:54 +0530

    added login API in signin component

--- client/src/components/SignInUp/SignIn.js
+++ client/src/components/SignInUp/SignIn.js
@@ -135,12 +135,24 @@ import React, { useState } from "react";
 import './SignInStyles.css'
 
 const SignIn = (props) => {
+    const host="http://localhost:5000"
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
 
-    const handleSubmit = (e) => {
+    async function handleSubmit(e){
         e.preventDefault();
-        console.log(email);
+        // console.log(email,pass);
+        const response = await fetch(`${host}/api/auth/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            },
+          body: JSON.stringify({email:email,password:pass}) 
+        });
+
+        const json=response.json()
+        console.log(json)
     //     catch the input from here
     }
 
@@ -150,12 +162,12 @@ const SignIn = (props) => {
             <form className="login-form" onSubmit={handleSubmit}>
                 <h2>Sign In</h2>
                 <label htmlFor="email">email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
                 <label htmlFor="password">password</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
                 <button type="submit">Log In</button>
             </form>
-            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}><strong>Don't have an account? Register here</strong></button>
         </div>
         </div>
     )
--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express=require("express");
 const body_parser=require("body-parser");
 const mongoose=require("mongoose");
+const cors=require('cors');
 
 const app=express();
 
+app.use(cors())
 app.use(express.json())
 mongoose.set('strictQuery', true);
 mongoose.connect("mongodb://127.0.0.1/stockdb",{useNewUrlParser:true});
-
 
 app.use(body_parser.urlencoded({extended:true}));
 
