64660b6c42e78b82ab3aba7c196688f5b3e66e72
Arpit Mathur <arpitmathur23@gmail.com>
2023-04-13 13:12:06 +0530

    navbar changed

---
+++ client/src/components/Navbar/Navbarf.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import './NavbarStyles.css'
+import {MenuItems} from "./MenuItems";
+import {Link} from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button'
+
+export const Navbarf = () => {
+    const navigate=useNavigate();
+    const [state,setState] = useState({clicked : false})
+    const handleClick = () => {
+       
+        setState({clicked : !state.clicked})
+    }
+       
+    function onLogout(e){
+        e.preventDefault();
+        localStorage.removeItem('token');
+        navigate("/");
+    }
+        return (
+            
+            <nav className='NavbarItems'>
+                <h1 className='navbar-logo'>StockWise</h1>
+
+                <div className="menu-icons" onClick={handleClick}>
+                    <i className={state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+
+                <ul className={state.clicked? 'nav-menu active' : 'nav-menu'}>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <Link className={item.cName} to={item.url}>
+                                    {item.title}
+                                </Link>
+                            </li>
+                        );
+                    })}
+                    {!localStorage.getItem('token')?<Link className={'sign-in-up'} to={'/signin'}>Sign In</Link>:<Button className='sign-in-up' onClick={onLogout}>Logout</Button>}
+                </ul>
+            </nav>
+        )
+    }
+export default Navbarf;
