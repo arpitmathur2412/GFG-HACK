2daa75eba3f2b26876a09ac506973f1b6eb67703
Arpit Mathur <arpitmathur23@gmail.com>
2023-05-16 03:01:11 +0530

    docker-compose created

--- server/index.js
+++ server/index.js
@@ -13,7 +13,6 @@ mongoose.connect("mongodb://127.0.0.1/stockdb",{useNewUrlParser:true});
 app.use(body_parser.urlencoded({extended:true}));
 
 app.use("/api/auth",require("./routes/auth"))
-// app.use("/api/notes",require("./routes/notes"))
 
 app.get("/",(req,res)=>{
     res.send("hello")
